aeece57daeab858c582ba5d4fa2bec2fd73ec452
Ankur Prajapati <91510065+Ankur77720@users.noreply.github.com>
2025-08-28 18:39:59 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const app = express()
+
+app.use(express.json())
+/* note = { title:string,description:string } */
+
+const notes = []
+
+app.post("/notes", (req, res) => {
+    notes.push(req.body)
+    res.send("note added successfully")
+})
+
+app.get("/notes", (req, res) => {
+    res.send(notes)
+})
+
+app.delete("/notes/:index", (req, res) => {
+    const index = req.params.index
+    delete notes[ index ]
+    res.send("note deleted successfully")
+})
+
+app.patch("/notes/:index", (req, res) => {
+    const index = req.params.index
+    const description = req.body.description
+
+    notes[ index ].description = description
+
+    res.send("note updated successfully")
+})
+
+app.listen(3000, () => {
+    console.log("server is running on port 3000");
+})
